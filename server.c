c52994efcd19a44380c1af55a79437374e4e9b1e
yahiarrh <107009858+yahiarrh@users.noreply.github.com>
2023-03-10 13:20:59 +0100

    update

--- server.c
+++ server.c
@@ -12,20 +12,37 @@
 
 #include "minitalk.h"
 
-static void handler (int signal, siginfo_t *pid, void *s)
+static int	ft_putchar(char c)
 {
-	static char	c = 0;
-	static int	i = 0;
+	write(1, &c, 1);
+	return (1);
+}
+static void handler (int signal, siginfo_t *info, void *s)
+{
+	static unsigned char	c;
+	static int	i;
+	static pid_t	client_pid;
 
-	(void)pid;
-	(void)s;
-	c |= (signal == SIGUSR1);
-	if (++i == 8)
+	i++;
+	(void)s; 
+	if (!client_pid)
+		client_pid = info->si_pid;
+	if (client_pid != info->si_pid)
 	{
-	 		
+		client_pid = info->si_pid;
+		c = 0;
+		i = 0;
+	}
+	if (signal == SIGUSR1);
+		c = (c | 1);
+	if (i == 8)
+	{
+	 	ft_printf("daz %c",c);
+		i = 0; 
+		c = 0;
 	}
 	else
-		c <<= i;
+		c = c << 1;
 }
 
 int main(int ac, char **av)
@@ -33,15 +50,18 @@ int main(int ac, char **av)
 	struct sigaction sa;
 
 	(void)av;
-	sa.sa_sigaction = handler;
 	if(ac != 1)
 	{
 		ft_printf("Wrong parameters try : \n[./server]");
 	}
+	sa.sa_sigaction = handler;
+	sa.sa_flags = SA_SIGINFO;
 	ft_printf("PID : %d \n", getpid());
-	sigaction(SIGUSR1, &sa, NULL);
-	sigaction(SIGUSR2, &sa, NULL);
 	while (1)
+	{
+		sigaction(SIGUSR2, &sa, NULL);
+		sigaction(SIGUSR1, &sa, NULL);
 		pause();
+	}
 	return(0);
 }
